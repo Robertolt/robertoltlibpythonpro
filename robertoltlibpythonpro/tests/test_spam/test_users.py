5abc661ce8fc6198a07026bb99ad948e73614660
robertolt <rl.beto.lorenzoni@gmail.com>
2021-04-27 18:56:57 -0300

    added invalid test

--- robertoltlibpythonpro/tests/test_spam/test_users.py
+++ robertoltlibpythonpro/tests/test_spam/test_users.py
@@ -1,25 +1,34 @@
+import pytest
+
 from robertoltlibpythonpro.spam.db import Connection
 from robertoltlibpythonpro.spam.modules import User
 
 
-def test_save_user():
-    connection = Connection()
-    session = connection.generate_session()
+@pytest.fixture
+def connection():
+    connection_obj = Connection()
+    yield connection_obj
+    connection_obj.close()
+
+
+@pytest.fixture
+def session(connection):
+    session_obj = connection.generate_session()
+    yield session_obj
+    session_obj.roll_back()
+    session_obj.close()
+
+
+def test_save_user(session):
     user = User(name='Roberto')
     session.save(user)
     assert isinstance(user.id, int)
-    session.roll_back()
-    session.close()
-    connection.close()
 
 
-def test_list_user():
-    connection = Connection()
-    session = connection.generate_session()
+
+
+def test_list_user(session):
     users = [User(name='Roberto'), User(name='Janine')]
     for user in users:
         session.save(user)
     assert users == session.list()
-    session.roll_back()
-    session.close()
-    connection.close()
